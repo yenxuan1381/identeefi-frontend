3d458026ab23031de72247a9b406b629885c9ce4
yenxuan1381 <emilylo1381@gmail.com>
2023-03-02 22:02:34 +0800

    done handleSubmit func

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,12 @@
 import '../../css/Login.css';
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from '../../context/AuthProvider';
 import { Button } from '../Button';
+import axios from '../../api/axios';
+const LOGIN_URL = '/auth';
 
 const Login = () => {
+    const { auth, setAuth } = useContext(AuthContext);
     const userRef = useRef();
     const errRef = useRef();
 
@@ -19,9 +23,50 @@ const Login = () => {
         setErrMsg('');
     }, [user, pwd]);
 
-    const handleSubmit = async e => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(user, pwd)
+
+        try {
+            const res = await axios.post(`${LOGIN_URL}/${user}/${pwd}`,
+                {
+                    headers: { 
+                        'Content-Type': 'application/json' 
+                    }
+                });
+            // console.log(JSON.stringify(res?.data));
+            console.log(res?.data);
+
+            if (res?.data.length === 0) {
+                setErrMsg('Login Failed. Please try again later.');
+                errRef.current.focus();
+            } else {
+                const role = res?.data?.role;
+    
+                setAuth({ user, pwd, role})
+                setUser('');
+                setPwd('');
+                setSuccess(true);
+
+            }
+
+
+
+
+        } catch (err) {
+            if(!err?.response) {
+                setErrMsg('Server is down. Please try again later.');
+            } else if (err.response?.status === 400) {
+                setErrMsg('Invalid username or password.');
+            } else if (err.response?.status === 401){
+                setErrMsg('Unauthorized access.');
+            } else {
+                setErrMsg('Login Failed. Please try again later.');
+            }
+            errRef.current.focus();
+        }
+
+
     };
 
     return (
@@ -32,26 +77,26 @@ const Login = () => {
                 <h2>Login</h2>
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="username">Username</label>
-                    <input 
-                        type="text" 
-                        id="username" 
+                    <input
+                        type="text"
+                        id="username"
                         ref={userRef}
                         onChange={(e) => setUser(e.target.value)}
                         value={user}
-                        required                        
+                        required
                     />
                     <br />
                     <label htmlFor="password">Password</label>
-                    <input 
-                        type="password" 
-                        id="password" 
+                    <input
+                        type="password"
+                        id="password"
                         onChange={(e) => setPwd(e.target.value)}
                         value={pwd}
-                        required                        
+                        required
                     />
                     <Button className="btn" buttonStyle='btn--primary' buttonSize='btn--medium'>Login</Button>
                 </form>
-                
+
 
             </div>
         </div>
